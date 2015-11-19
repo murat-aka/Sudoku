1749011d6a34449de5398a64b34c5e6cd49a9138
murat-aka <murat-aka@hotmail.com>
2015-11-19 17:59:06 +0000

    tutorial 2

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -10,14 +10,8 @@ package sudoku;
  * @author murat
  */
 public class Sudoku {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-         int userGrid[][]=new int[][]
-           {{0,0,0,0,0,0,0,0,0},
+ static int userGrid[][]=new int[][]
+           {{1,0,0,0,0,0,0,0,0},
             {0,0,0,0,0,0,0,0,0},
             {0,0,0,0,0,0,0,0,0},
             {0,0,0,0,0,0,0,0,0},
@@ -26,12 +20,29 @@ public class Sudoku {
             {0,0,0,0,0,0,0,0,0},
             {0,0,0,0,0,0,0,0,0},
             {0,0,0,0,0,0,0,0,0}};//[horizontal][vertical]
-        int grid[][]=new int[9][9];//the grid that the program experiments on
-        String pgrid[][]=new String[9][9];//the posibilities grid
+        static int grid[][]=new int[9][9];//the grid that the program experiments on
+        //static String pgrid[][]=new String[9][9];//the posibilities grid
+    public static void main(String[] args) {
+        for (int i=0;i<9;i++) {
+            for (int j=0;j<9;j++) {
+                grid[i][j]=userGrid[i][j];
+            }
+        }
+        System.out.println(validity(0, 0, grid));
         print(grid);
     }
-
-    
+    public static boolean validity(int x, int y, int[][] grid) {
+        String temp="";
+        for (int i=0;i<9;i++) {
+            temp+=Integer.toString(grid[i][y]);//horizontal
+            temp+=Integer.toString(grid[x][i]);//verical
+            temp+=Integer.toString(grid[(x/3)*+i/3][(y/3)*3+i%3]);//square
+        }
+        int count=0, idx=0;
+        while ((idx=temp.indexOf(Integer.toString(grid[x][y]), idx))!=-1)
+        {idx++; count++;}
+        return count==3;
+    }
     public static void print(int[][] grid) {
         System.out.println();
         for (int i=0;i<9;i++) {
